f29883762cc4aa92b13a9ca18b39cd344968c045
0xnbk <github@nbk.email>
2024-02-21 20:40:07 +0530

    initialised home html content with twind styling

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,25 @@
-import { Hono } from 'hono'
-import { serveStatic } from 'hono/cloudflare-workers'
 // @ts-ignore
 import manifest from '__STATIC_CONTENT_MANIFEST'
+import { Hono } from 'hono'
+import { serveStatic } from 'hono/cloudflare-workers'
+import { Home } from './home'
+import { extract, install } from '@twind/core'
+import presetTailwind from '@twind/preset-tailwind'
+import { HtmlEscapedString } from 'hono/utils/html'
+
+install(
+    {
+        presets: [
+            presetTailwind()
+        ],
+    }
+)
+
+async function ssrTailwind(body: HtmlEscapedString | Promise<HtmlEscapedString>) {
+  const { html, css } = extract((await body).toString())
+
+  return html.replace('</head>', `<style data-twind>${css}</style></head>`)
+}
 
 const app = new Hono()
 
@@ -9,8 +27,12 @@ app.use('/*', serveStatic({ root: './', manifest }))
 
 app.get('/favicon.ico', serveStatic({  path: './favicon.ico', manifest }))
 
+app.post('/url', (c) => {
+  const url =  c.req.parseBody()
+  return c.text('Test')
+})
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.html(ssrTailwind(Home))
 })
 
 export default app
