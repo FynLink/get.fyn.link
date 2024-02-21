7f977b90f56504c029445fe30f261e5d940816d0
0xnbk <github@nbk.email>
2024-02-21 10:09:28 +0530

    serve statics files functionality implemented

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import { Hono } from 'hono'
+import { serveStatic } from 'hono/cloudflare-workers'
+// @ts-ignore
+import manifest from '__STATIC_CONTENT_MANIFEST'
 
 const app = new Hono()
+
+app.use('/*', serveStatic({ root: './', manifest }))
+
+app.get('/favicon.ico', serveStatic({  path: './favicon.ico', manifest }))
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
